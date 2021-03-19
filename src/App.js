3bfcd3eade5d65fb8787421942b4d60ca5e9b8fc
bunyaminfil <bunyaminfl@gmail.com>
2021-03-19 12:18:43 +0300

    Loading

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   state = {
-    users: []
+    users: [],
+    isLoading: true
   }
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users").then(data => data.json()).then(users => {
-      this.setState({
-        users //users:users
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/users").then(data => data.json()).then(users => {
+        this.setState({
+          users, //users:users
+          isLoading: false
+        })
+        console.log(users);
       })
-      console.log(users);
-    })
+    }, 3000);
   }
 
   render() {
+    const { isLoading } = this.state
     return (
       <div className="App">
         <h1>Users</h1>
+        {isLoading ? "Loading..." : ""}
         {
           this.state.users.map(user =>
             <div key={user.id} className={"userList"}>{user.name} - @{user.username}</div>
